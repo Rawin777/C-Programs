1e5e222473f1bac94b475bda3b4c3523ec9aa3df
Jatin Rawat <jatinyrawat06@gmail.com>
2026-03-26 15:01:05 +0530

    Add files via upload

---
+++ 07_decrementing_for_loop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i=5; i ; i--){ // runs until i becomes zero and hence becomes false thus terminating the loop.
+        printf("%d\n",i);
+    }
+    return 0;
+}
